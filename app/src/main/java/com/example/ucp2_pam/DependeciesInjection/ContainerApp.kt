e23116abbe9f14e855db7499497d9ac099b4c8b0
Jantopthegreat <fauzangriya@gmail.com>
2024-12-19 22:39:38 +0700

    membuat containerApp

--- app/src/main/java/com/example/ucp2_pam/DependeciesInjection/ContainerApp.kt
+++ app/src/main/java/com/example/ucp2_pam/DependeciesInjection/ContainerApp.kt
@@ -1,6 +1,9 @@
 package com.example.ucp2_pam.DependeciesInjection
 
 import android.content.Context
+import com.example.ucp2_pam.Data.Database.DatabaseBrgSpl
+import com.example.ucp2_pam.Repository.LocalRepositoryBrg
+import com.example.ucp2_pam.Repository.LocalRepositorySpl
 import com.example.ucp2_pam.Repository.RepositoryBrg
 import com.example.ucp2_pam.Repository.RepositorySpl
 
@@ -13,6 +16,11 @@ val repositorySpl : RepositorySpl
 
 class ContainerApp(private val context: Context) : InterfaceContainerApp{
     override val repositorySpl: RepositorySpl by lazy {
+        LocalRepositorySpl(DatabaseBrgSpl.getDatabase(context).supplierDao())
+    }
+
+        override val repositoryBrg: RepositoryBrg by lazy {
+            LocalRepositoryBrg(DatabaseBrgSpl.getDatabase(context).barangDao())
 
     }
 

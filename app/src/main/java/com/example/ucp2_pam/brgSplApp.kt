6297f1f53414b5d7c0ea4ddcad7db8e6e16c9ef4
Jantopthegreat <fauzangriya@gmail.com>
2024-12-19 22:45:59 +0700

    Membuat fungsi onCreate pada Application

--- app/src/main/java/com/example/ucp2_pam/brgSplApp.kt
+++ app/src/main/java/com/example/ucp2_pam/brgSplApp.kt
@@ -1,2 +1,18 @@
 package com.example.ucp2_pam
 
+import android.app.Application
+import com.example.ucp2_pam.DependeciesInjection.ContainerApp
+
+class brgSplApp: Application() {
+
+    lateinit var containerApp: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        //Membuat interface container app
+
+        containerApp = ContainerApp(this)
+        //Instance adalah object yang dibuat dari class
+    }
+
+}

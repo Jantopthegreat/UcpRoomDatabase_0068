3e96b3226086c6e01b015d2e1e8b82645c0a41c2
Jantopthegreat <fauzangriya@gmail.com>
2024-12-20 15:26:28 +0700

    membuat UpdateState untuk Supplier

--- app/src/main/java/com/example/ucp2_pam/ui/viewmodel/SupplierViewModel.kt
+++ app/src/main/java/com/example/ucp2_pam/ui/viewmodel/SupplierViewModel.kt
@@ -1,7 +1,22 @@
 package com.example.ucp2_pam.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
 import com.example.ucp2_pam.Data.Entity.Supplier
+import com.example.ucp2_pam.Repository.RepositoryBrg
+import com.example.ucp2_pam.Repository.RepositorySpl
 
+class SupplierViewModel (private val repositorySpl: RepositorySpl): ViewModel() {
+
+    var uiState by mutableStateOf(SupplierUiState())
+
+    fun updateState(barangEvent: BarangEvent){
+        uiState = uiState.copy(
+            supplierEvent = supplierEvent,
+        )
+    }
 
 
 data class SupplierFormErrorState(

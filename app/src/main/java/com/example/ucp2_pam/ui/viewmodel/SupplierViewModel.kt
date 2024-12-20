7f09678da737d68747b8afeac62b6cf9cd69024b
Jantopthegreat <fauzangriya@gmail.com>
2024-12-20 15:23:16 +0700

    membuat FormErrorState untuk Supplier

--- app/src/main/java/com/example/ucp2_pam/ui/viewmodel/SupplierViewModel.kt
+++ app/src/main/java/com/example/ucp2_pam/ui/viewmodel/SupplierViewModel.kt
@@ -3,6 +3,21 @@ package com.example.ucp2_pam.ui.viewmodel
 import com.example.ucp2_pam.Data.Entity.Supplier
 
 
+
+data class SupplierFormErrorState(
+    val id: String? = null,
+    val nama_spl: String? = null,
+    val kontak: String? = null,
+    val alamat: String? = null
+
+) {
+
+    fun isValid(): Boolean{
+        return id == null && nama_spl == null && kontak == null &&
+                alamat == null
+    }
+}
+
 data class SupplierUiState(
     val supplierEvent: SupplierEvent,
     val isEntryValid: SupplierFormErrorState = SupplierFormErrorState(),

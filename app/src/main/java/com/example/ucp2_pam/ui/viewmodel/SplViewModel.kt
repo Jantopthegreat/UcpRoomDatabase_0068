29261e330e0ba81bc7883dea2310e934ee7d7d75
Jantopthegreat <fauzangriya@gmail.com>
2024-12-20 13:18:17 +0700

    perbaikan penamaan dan penambahan untuk class untuk viewmodel Supplier

--- app/src/main/java/com/example/ucp2_pam/ui/viewmodel/SplViewModel.kt
+++ app/src/main/java/com/example/ucp2_pam/ui/viewmodel/SplViewModel.kt
@@ -1,9 +1,54 @@
 package com.example.ucp2_pam.ui.viewmodel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2_pam.Data.Entity.Barang
+import com.example.ucp2_pam.Data.Entity.Supplier
+import com.example.ucp2_pam.Repository.RepositoryBrg
+import com.example.ucp2_pam.Repository.RepositorySpl
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class SplViewModel ( private val repositorySpl: RepositorySpl) : ViewModel() {
+
+    val splUiState: StateFlow<SplUiState> = repositorySpl.getAllSpl()
+        .filterNotNull()
+        .map {
+            SplUiState(
+                listSpl = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit (SplUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                SplUiState(
+                    isLoading = false,
+                    isError = false,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = SplUiState(
+                isLoading = true,
+            )
+        )
+}
 
 data class SplUiState (
-    val listBrg : List<Barang> = listOf(),
+    val listSpl : List<Supplier> = listOf(),
     val isLoading: Boolean = false,
     val isError: Boolean = false,
     val errorMessage: String = ""

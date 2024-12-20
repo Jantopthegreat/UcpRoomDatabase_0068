5452ba5ca4ba8530e529d409f079479d305641e2
Jantopthegreat <fauzangriya@gmail.com>
2024-12-21 02:42:37 +0700

    menambah classviewmodel untuk DetialBarang

--- app/src/main/java/com/example/ucp2_pam/ui/viewmodel/DetailBrgViewModel.kt
+++ app/src/main/java/com/example/ucp2_pam/ui/viewmodel/DetailBrgViewModel.kt
@@ -2,9 +2,19 @@ package com.example.ucp2_pam.ui.viewmodel
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2_pam.Data.Entity.Barang
 import com.example.ucp2_pam.Repository.RepositoryBrg
 import com.example.ucp2_pam.ui.navigation.DestinasiDetailBrg
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 
 class DetailBrgViewModel (
@@ -14,6 +24,43 @@ class DetailBrgViewModel (
     ) : ViewModel() {
     private val id_brg: String = checkNotNull(savedStateHandle[DestinasiDetailBrg.ID])
 
+
+    val detailBrgUiState: StateFlow<DetailBrgUiState> = repositoryBrg.getBrg(id_brg)
+        .filterNotNull()
+        .map {
+            DetailBrgUiState(
+                detailBrgUiEvent = it.toDetailBrgUiEvent(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(DetailBrgUiState(isLoading = true))
+            delay(600)
+        }
+        .catch {
+            emit(
+                DetailBrgUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi kesalahan",
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DetailBrgUiState(
+                isLoading = true,
+            ),
+        )
+    fun deleteBrg() {
+        detailBrgUiState.value.detailBrgUiEvent.toBarangEntity().let {
+            viewModelScope.launch {
+                repositoryBrg.deleteBrg(it)
+            }
+        }
+    }
+}
 
 
 data class DetailBrgUiState(

e43309221958c0490f4e629443781e333b23251e
Jantopthegreat <fauzangriya@gmail.com>
2024-12-20 15:47:02 +0700

    membuat validasi kolom dan save data untuk supplier

--- app/src/main/java/com/example/ucp2_pam/ui/viewmodel/SupplierViewModel.kt
+++ app/src/main/java/com/example/ucp2_pam/ui/viewmodel/SupplierViewModel.kt
@@ -4,19 +4,67 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2_pam.Data.Entity.Supplier
 import com.example.ucp2_pam.Repository.RepositoryBrg
 import com.example.ucp2_pam.Repository.RepositorySpl
+import kotlinx.coroutines.launch
 
 class SupplierViewModel (private val repositorySpl: RepositorySpl): ViewModel() {
 
     var uiState by mutableStateOf(SupplierUiState())
 
-    fun updateState(barangEvent: BarangEvent){
+    fun updateState(supplierEvent: SupplierEvent){
         uiState = uiState.copy(
             supplierEvent = supplierEvent,
+
         )
     }
+
+    fun validateFields(): Boolean{
+        val event = uiState.supplierEvent
+        val errorState = SupplierFormErrorState(
+            id = if (event.id.isNotEmpty()) null else "ID tidak boleh kosong",
+            nama_spl = if (event.nama_spl.isNotEmpty()) null else "Nama Supplier tidak boleh kosong",
+            kontak = if (event.kontak.isNotEmpty()) null else "kontak tidak boleh kosong",
+            alamat = if (event.alamat.isNotEmpty()) null else "Alamat tidak boleh kosong",
+        )
+        uiState = uiState.copy(isEntryValid = errorState)
+        return errorState.isValid()
+
+    }
+    fun saveData(){
+        val currentEvent = uiState.supplierEvent
+
+        if (validateFields()){
+            viewModelScope.launch{
+                try {
+                    repositorySpl.insertSpl(currentEvent.toSupplierEntity())
+                    uiState = uiState.copy(
+                        snackBarMessage = "Data Berhasil Disimpan",
+                        supplierEvent = SupplierEvent(),
+                        isEntryValid = SupplierFormErrorState()
+                    )
+
+                }
+                catch (e: Exception) {
+                    uiState = uiState.copy(
+                        snackBarMessage = " Data gagal Disimpan"
+                    )
+                }
+            }
+        } else {
+            uiState = uiState.copy(
+                snackBarMessage = " Input tidak valid Periksa kembali Data Anda!"
+            )
+        }
+    }
+
+    fun resetSnackBarMessage(){
+        uiState = uiState.copy(snackBarMessage = null)
+    }
+}
+
 
 
 data class SupplierFormErrorState(
@@ -28,13 +76,15 @@ data class SupplierFormErrorState(
 ) {
 
     fun isValid(): Boolean{
-        return id == null && nama_spl == null && kontak == null &&
-                alamat == null
+        return id == null
+                && nama_spl == null
+                && kontak == null
+                && alamat == null
     }
 }
 
 data class SupplierUiState(
-    val supplierEvent: SupplierEvent,
+    val supplierEvent: SupplierEvent = SupplierEvent(),
     val isEntryValid: SupplierFormErrorState = SupplierFormErrorState(),
     val snackBarMessage: String? = null
 )

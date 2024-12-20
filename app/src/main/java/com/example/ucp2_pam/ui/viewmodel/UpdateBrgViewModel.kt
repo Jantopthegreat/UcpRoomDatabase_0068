86b41c832ca1a41d26d5e497ee8154638354ab2d
Jantopthegreat <fauzangriya@gmail.com>
2024-12-21 03:21:33 +0700

    menambahkan validasi kolom kosong dan updateBarang

--- app/src/main/java/com/example/ucp2_pam/ui/viewmodel/UpdateBrgViewModel.kt
+++ app/src/main/java/com/example/ucp2_pam/ui/viewmodel/UpdateBrgViewModel.kt
@@ -34,10 +34,58 @@ class UpdateMhsViewModel (
 
     fun updateState(barangEvent: BarangEvent) {
         updateUIState = updateUIState.copy(
-            barangEvent = BarangEvent,
+            barangEvent = barangEvent,
         )
     }
 
+    fun validateFields(): Boolean {
+        val event = updateUIState.barangEvent
+        val errorState = BarangFormErrorState(
+            id_brg = if (event.id_brg.isNotEmpty()) null else "ID tidak boleh kosong",
+            nama_brg = if (event.nama_brg.isNotEmpty()) null else "Nama Barang tidak boleh kosong",
+            deskripsi = if (event.deskripsi.isNotEmpty()) null else "Deskripsi tidak boleh kosong",
+            harga = if (event.harga.isNotEmpty()) null else "Harga tidak boleh kosong",
+            stok = if (event.stok > 0) null else "Stok tidak boleh kosong atau kurang dari 1",
+            nama_Suplier = if (event.nama_Suplier.isNotEmpty()) null else "Nama Supplier tidak boleh kosong",
+        )
+
+        updateUIState = updateUIState.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+    fun updateData() {
+        val currentEvent = updateUIState.barangEvent
+
+        if (validateFields()) {
+            viewModelScope.launch {
+                try {
+                    repositoryBrg.updateBrg(currentEvent.toBarangEntity())
+                    updateUIState = updateUIState.copy(
+                        snackBarMessage = "Data berhasil diupdate",
+                        barangEvent = BarangEvent(),
+                        isEntryValid = BarangFormErrorState()
+                    )
+                    println(
+                        "snackBarMessage diatur: ${
+                            updateUIState.snackBarMessage
+                        }"
+                    )
+                } catch (e: Exception) {
+                    updateUIState = updateUIState.copy(
+                        snackBarMessage = "Data gagal diupdate"
+                    )
+                }
+            }
+        } else {
+            updateUIState = updateUIState.copy(
+                snackBarMessage = "Data gagal diupdate"
+            )
+        }
+    }
+
+    fun resetSnackBarMessage() {
+        updateUIState = updateUIState.copy(snackBarMessage = null)
+    }
+}
 
 
 fun Barang.toUiStateBrg () : BarangUiState = BarangUiState (
